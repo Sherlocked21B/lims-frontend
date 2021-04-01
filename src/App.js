7db02bb6fed9f7c866cc750389a7432216a234f6
Sherlocked21B <bijayalamichhane13@gmail.com>
2021-04-01 15:05:54 +0545

    Generate Report completed

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ import AllSample from "./Components/AllSample";
 import AllReagent from "./Components/AllReagent";
 import AddSample from "./Components/AddSample";
 import GenerateReport from "./Components/GenerateReport";
+import Report from "./Components/report";
 import AllTest from "./Components/AllTest";
 import EditTest from "./Components/EditTest";
 import ProtectedRoute from "./Components/ProtectedRoute";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
@@ -25,44 +22,45 @@ import Register from "./Components/register";
 import { setUser } from "./action/setUser";
 
 function App() {
-  const dispatch = useDispatch();
-  const token = localStorage.getItem("token");
-  const [isLogin, setLogin] = useState(token ? true : false);
+	const dispatch = useDispatch();
+	const token = localStorage.getItem("token");
+	const [isLogin, setLogin] = useState(token ? true : false);
 
-  if (token) {
-    try {
-      const decoded = jwt_decode(token);
-      dispatch(setUser(decoded.id, decoded.role));
-    } catch (e) {
-      localStorage.removeItem("token");
-      setLogin(false);
-    }
-  }
+	if (token) {
+		try {
+			const decoded = jwt_decode(token);
+			dispatch(setUser(decoded.id, decoded.role));
+		} catch (e) {
+			localStorage.removeItem("token");
+			setLogin(false);
+		}
+	}
 
-  return isLogin ? (
-    <Router>
-      <div className="App">
-        <AppBar />
-        <Switch>
-          <Route path="/" exact component={PendingSample} />
-          <Route path="/addCustomer" component={AddCustomer} />
-          <Route path="/addReagent" component={AddReagent} />
-          <Route path="/addTest" component={AddTest} />
-          <Route path="/importReagent" component={ImportReagent} />
-          <Route path="/allSample" component={AllSample} />
-          <ProtectedRoute path="/allReagent" component={AllReagent} />
-          <Route path="/allTest" component={AllTest} />
-          <Route path="/addSample" component={AddSample} />
-          <Route path="/register" exact component={Register} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/generateReport" exact component={GenerateReport} />
-          <Route path="/editTest" exact component={EditTest} />
-        </Switch>
-      </div>
-    </Router>
-  ) : (
-    <Login isLogin={isLogin} setLogin={setLogin} />
-  );
+	return isLogin ? (
+		<Router>
+			<div className="App">
+				<AppBar />
+				<Switch>
+					<Route path="/" exact component={PendingSample} />
+					<Route path="/addCustomer" component={AddCustomer} />
+					<Route path="/addReagent" component={AddReagent} />
+					<Route path="/addTest" component={AddTest} />
+					<Route path="/importReagent" component={ImportReagent} />
+					<Route path="/allSample" component={AllSample} />
+					<Route path="/allReagent" component={AllReagent} />
+					<Route path="/allTest" component={AllTest} />
+					<Route path="/addSample" component={AddSample} />
+					<Route path="/register" exact component={Register} />
+					<Route path="/login" exact component={Login} />
+					<Route path="/generateReport" exact component={GenerateReport} />
+					<Route path="/editTest" exact component={EditTest} />
+					<Route path="/report" exact component={Report} />
+				</Switch>
+			</div>
+		</Router>
+	) : (
+		<Login isLogin={isLogin} setLogin={setLogin} />
+	);
 }
 
 export default App;
--- src/Components/appBar.js
+++ src/Components/appBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import {
-  makeStyles,
-  MenuItem,
-  Drawer,
-  AppBar,
-  Toolbar,
-  CssBaseline,
-  Typography,
-  Divider,
-  IconButton,
-  Container,
-  Button,
+	makeStyles,
+	MenuItem,
+	Drawer,
+	AppBar,
+	Toolbar,
+	CssBaseline,
+	Typography,
+	Divider,
+	IconButton,
+	Container,
+	Button,
 } from "@material-ui/core";
 import CollectionsBookmarkIcon from "@material-ui/icons/CollectionsBookmark";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -27,164 +27,175 @@ import { useSelector } from "react-redux";
 const drawerWidth = 290;
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
+	root: {
+		display: "flex",
+	},
 
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: "nowrap",
-  },
-  iconSpacing: {
-    display: "flex",
-    marginLeft: theme.spacing(3),
-  },
-  backButton: {
-    display: "flex",
-    justifyContent: "center",
-  },
-  paragraph: {
-    fontSize: 18,
-    marginLeft: "5px",
-  },
-  container: {
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-  title: {
-    width: "700%",
-  },
-  lims: {},
+	menuButton: {
+		marginRight: 36,
+	},
+	hide: {
+		display: "none",
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: "nowrap",
+	},
+	iconSpacing: {
+		display: "flex",
+		marginLeft: theme.spacing(3),
+	},
+	backButton: {
+		display: "flex",
+		justifyContent: "center",
+	},
+	paragraph: {
+		fontSize: 18,
+		marginLeft: "5px",
+	},
+	container: {
+		display: "flex",
+		justifyContent: "flex-end",
+	},
+	title: {
+		width: "700%",
+	},
+	lims: {},
 }));
 
 export default function Apppbar(props) {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const state = useSelector((state) => state.auth);
-  const handleDrawer = () => {
-    setOpen(!open);
-  };
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar className={classes.appbar}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            color="inherit"
-            size="medium"
-            edge="start"
-            onClick={handleDrawer}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Container className={classes.title}>
-            <Typography align="center" className={classes.lims}>
-              LABORATORY INFORMATION MANAGEMENT SYSTEM
-            </Typography>
-          </Container>
-          <Container className={classes.container}>
-            <Typography>
-              <IconButton color="inherit" size="medium">
-                <AccountCircleIcon />
-                <p className={classes.paragraph}> {state[0].role}</p>
-              </IconButton>
-            </Typography>
-            <Button
-              variant="contained"
-              color="secondary"
-              style={{ margin: "20px 4px 20px 4px" }}
-              onClick={() => {
-                localStorage.removeItem("token");
-                window.location.assign("/login");
-              }}
-            >
-              Logout
-            </Button>
-          </Container>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="persistent"
-        className={classes.root}
-        open={open}
-        classes={{ paper: classes.drawer }}
-      >
-        <MenuItem onClick={handleDrawer}>
-          <Container className={classes.backButton}>
-            <ArrowBackIosIcon />
-          </Container>
-        </MenuItem>
-        <Divider />
-        <MenuItem>
-          <CollectionsBookmarkIcon />
-          <Typography className={classes.iconSpacing}>Test</Typography>
-        </MenuItem>
-        <Divider />
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+	const state = useSelector((state) => state.auth);
+	const handleDrawer = () => {
+		setOpen(!open);
+	};
+	return (
+		<React.Fragment>
+			<style>{`@media print {.no-print{display: none;}}`}</style>
+			<div className={(classes.root, "no-print")}>
+				<CssBaseline />
+				<AppBar className={classes.appbar}>
+					<Toolbar className={classes.toolbar}>
+						<IconButton
+							color="inherit"
+							size="medium"
+							edge="start"
+							onClick={handleDrawer}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Container className={classes.title}>
+							<Typography align="center" className={classes.lims}>
+								LABORATORY INFORMATION MANAGEMENT SYSTEM
+							</Typography>
+						</Container>
+						<Container className={classes.container}>
+							<Typography>
+								<IconButton color="inherit" size="medium">
+									<AccountCircleIcon />
+									<p className={classes.paragraph}> {state[0].role}</p>
+								</IconButton>
+							</Typography>
+							<Button
+								variant="contained"
+								color="secondary"
+								style={{ margin: "20px 4px 20px 4px" }}
+								onClick={() => {
+									localStorage.removeItem("token");
+									window.location.assign("/login");
+								}}
+							>
+								Logout
+							</Button>
+						</Container>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="persistent"
+					className={classes.root}
+					open={open}
+					classes={{ paper: classes.drawer }}
+				>
+					<MenuItem onClick={handleDrawer}>
+						<Container className={classes.backButton}>
+							<ArrowBackIosIcon />
+						</Container>
+					</MenuItem>
+					<Divider />
+					<MenuItem>
+						<CollectionsBookmarkIcon />
+						<Typography className={classes.iconSpacing}>Test</Typography>
+					</MenuItem>
+					<Divider />
 
-        <MenuItem onClick={handleDrawer} component={Link} to={"/"}>
-          <HourglassEmptyIcon />
-          <Typography className={classes.iconSpacing}>
-            Pending Sample
-          </Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/addCustomer"}>
-          <PersonAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>Add Customer</Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/addSample"}>
-          <PostAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>Add Sample </Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/addTest"}>
-          <PostAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>Add Test </Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/allTest"}>
-          <AllInboxRoundedIcon />
-          <Typography className={classes.iconSpacing}>All Test </Typography>
-        </MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/"}>
+						<HourglassEmptyIcon />
+						<Typography className={classes.iconSpacing}>
+							Pending Sample
+						</Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/addCustomer"}>
+						<PersonAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>
+							Add Customer
+						</Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/addSample"}>
+						<PostAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>Add Sample </Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/addTest"}>
+						<PostAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>Add Test </Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/allTest"}>
+						<AllInboxRoundedIcon />
+						<Typography className={classes.iconSpacing}>All Test </Typography>
+					</MenuItem>
 
-        <MenuItem onClick={handleDrawer} component={Link} to={"/allSample"}>
-          <AllInboxRoundedIcon />
-          <Typography className={classes.iconSpacing}>All sample </Typography>
-        </MenuItem>
-        <Divider />
-        {/* Inventory Management */}
-        <MenuItem>
-          <CollectionsBookmarkIcon />
-          <Typography className={classes.iconSpacing}>
-            Inventory Management
-          </Typography>
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleDrawer} component={Link} to={"/allReagent"}>
-          <PostAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>All Reagent </Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/addReagent"}>
-          <PostAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>Add Reagent</Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/importReagent"}>
-          <PostAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>
-            Import Reagent{" "}
-          </Typography>
-        </MenuItem>
-        <MenuItem onClick={handleDrawer} component={Link} to={"/register"}>
-          <PersonAddRoundedIcon />
-          <Typography className={classes.iconSpacing}>
-            Register Staff
-          </Typography>
-        </MenuItem>
-      </Drawer>
-    </div>
-  );
+					<MenuItem onClick={handleDrawer} component={Link} to={"/allSample"}>
+						<AllInboxRoundedIcon />
+						<Typography className={classes.iconSpacing}>All sample </Typography>
+					</MenuItem>
+					<Divider />
+					{/* Inventory Management */}
+					<MenuItem>
+						<CollectionsBookmarkIcon />
+						<Typography className={classes.iconSpacing}>
+							Inventory Management
+						</Typography>
+					</MenuItem>
+					<Divider />
+					<MenuItem onClick={handleDrawer} component={Link} to={"/allReagent"}>
+						<PostAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>
+							All Reagent{" "}
+						</Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/addReagent"}>
+						<PostAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>Add Reagent</Typography>
+					</MenuItem>
+					<MenuItem
+						onClick={handleDrawer}
+						component={Link}
+						to={"/importReagent"}
+					>
+						<PostAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>
+							Import Reagent{" "}
+						</Typography>
+					</MenuItem>
+					<MenuItem onClick={handleDrawer} component={Link} to={"/register"}>
+						<PersonAddRoundedIcon />
+						<Typography className={classes.iconSpacing}>
+							Register Staff
+						</Typography>
+					</MenuItem>
+				</Drawer>
+			</div>
+		</React.Fragment>
+	);
 }

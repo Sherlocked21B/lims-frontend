6a2ed905fd4f5a5d96fc55ee00be7c4d8b83a54c
memegod <anishniraula85@gmail.com>
2021-05-14 14:48:45 +0545

    snackbar

--- src/Components/SnackBar.js
+++ src/Components/SnackBar.js
@@ -1,21 +1,19 @@
-
-import React from "react";
-import Snackbar from "@material-ui/core/Snackbar";
-import MuiAlert from "@material-ui/lab/Alert";
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-  }
-
+	return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function SnackBar({ messege, open, handleClose, status }) {
-    return (
-      <div>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-          <Alert onClose={handleClose} severity={status}>
-            {messege}
-          </Alert>
-        </Snackbar>
-      </div>
-    );
-  }
+	return (
+		<div>
+			<Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+				<Alert onClose={handleClose} severity={status}>
+					{messege}
+				</Alert>
+			</Snackbar>
+		</div>
+	);
+}
